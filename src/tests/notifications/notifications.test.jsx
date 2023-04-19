44788969134ff364868b97929478d829ec464bd5
Campbell <tranbaolam26902@gmail.com>
2023-04-19 15:39:51 +0700

    Update notifications.test.jsx

--- src/tests/notifications/notifications.test.jsx
+++ src/tests/notifications/notifications.test.jsx
@@ -6,30 +6,29 @@ import { deleteAllNotifications, deletesNotification } from './deleteNotificatio
 it('Test get notifications api', async () => {
     const notifications = await getNotifications();
 
-    // eslint-disable-next-line jest/valid-expect
-    expect(notifications.length !== 0);
+    expect(Array.isArray(notifications)).toEqual(true);
 });
 
 it('Test read notification', async () => {
     const readNotification = await readsNotification();
 
-    expect(readNotification.id !== null);
+    expect(readNotification.Id).toBeDefined();
 });
 
 it('Test get all notifications read', async () => {
     const notificationsRead = await readAllNotifications();
 
-    expect(notificationsRead.length !== 0);
+    expect(Array.isArray(notificationsRead));
 });
 
 it('Test delete notification', async () => {
     const deleteNOtification = await deletesNotification();
 
-    expect(deleteNOtification.id !== null);
+    expect(deleteNOtification.Id).toBeDefined();
 });
 
 it('Test delete all notifications', async () => {
     const deleteAll = await deleteAllNotifications();
 
-    expect(deleteAll.length !== 0);
+    expect(Array.isArray(deleteAll)).toEqual(true);
 });
