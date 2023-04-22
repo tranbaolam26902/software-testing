0c514f9b5167139fcc3017193bc373c7c9b77e6b
2011416_nhan <2011416@dlu.edu.vn>
2023-04-22 16:45:56 +0700

    update

--- src/tests/categories/deleteCategory.jsx
+++ src/tests/categories/deleteCategory.jsx
@@ -2,8 +2,17 @@ import _fetch from 'isomorphic-fetch';
 import getToken from '../getToken';
 
 async function deleteCategory() {
-    return await fetch(process.env.REACT_APP_API_ENDPOINT + '/api/admcategory/delete', )
-            .then((response) => response.json())
+    const token = await getToken();
+    const formData = new FormData();
+    const idData = '276b64f0-bc4b-4ea6-9f38-f7cc6dde2397'
+    formData.append('id', JSON.stringify(idData));
+    return await fetch(process.env.REACT_APP_API_ENDPOINT + '/api/admcategory/delete',{
+        method: 'POST',
+                headers: {
+                    Authorization: localStorage.getItem('token').replace(/['"]+/g, ''),
+                },
+                body: formData,
+    }).then((response) => response.json())
 
 }
 
--- src/tests/profile/changePassword.jsx
+++ src/tests/profile/changePassword.jsx
@@ -2,7 +2,32 @@ import _fetch from 'isomorphic-fetch';
 import getToken from '../getToken';
 
 async function changePassword() {
-    return await fetch(process.env.REACT_APP_API_ENDPOINT + '/api/Account/ChangePassword').then((response) => response.json());
+    const token = await getToken();
+    const data = {
+                OldPassword: '',
+                NewPassword: '',
+                ConfirmPassword: '',
+            };
+    return await fetch(process.env.REACT_APP_API_ENDPOINT + '/api/Account/ChangePassword',{
+        method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: token,
+                },
+                body: JSON.stringify(data),
+    }).then((response) => {
+        if (response.status === 200) {
+            handleClose(e);
+            dispatch(actions.setMessage('Đổi mật khẩu thành công!'));
+            dispatch(actions.setShowMessageModal(true));
+            return;
+        }
+        if (response.status === 400) {
+            response.json().then((responseError) => {
+                setErrorMessage(responseError.ModelState.Error[0]);
+            });
+        }
+    });
 }
 
 export { changePassword };
